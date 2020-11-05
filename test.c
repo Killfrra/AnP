7c87df46f8653e431893419329c3e15755185bd0
Mak Kma <killfrra@gmail.com>
2020-11-05 20:37:27 +0300

    stabilized

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #define len(a) (sizeof(a) / sizeof(a[0]))
+#define connect(a, b)  { a->next = b; b->prev = a; }
+#define connect3(a, b, c) { connect(a, b); connect(b, c); }
 
 typedef struct list_element {
     int val;
@@ -13,8 +15,8 @@ ListElement l[] = {
     { 3, &l[2], &l[4] },
     { 0, &l[3], &l[5] },
     { 5, &l[4], &l[6] },
-    { 7, &l[5], &l[7] },
-    { 2, &l[6], NULL  }
+    { 7, &l[5], NULL  },
+    //{ 2, &l[5], NULL  }
 };
 
 int list_len = len(l);
@@ -40,40 +42,32 @@ typedef struct {
 
 Cut merge(Cut l1, Cut l2){
     Cut cut = { .next = l2.next };
-    ListElement * cur;
+    ListElement * cur, * cur1, * cur2;
     if(l1.first->val < l2.first->val){
-        cut.first = l1.first;
-        cur = l2.first;        
+        cut.first = cur = l1.first;
+        cur1 = l1.first->next;
+        cur2 = l2.first;
     } else {
-        cut.first = l2.first;
-        cur = l1.first;
+        cut.first = cur = l2.first;
+        cur1 = l1.first;
+        cur2 = l2.first->next;
     }
-
-    ListElement * cur1 = l1.first->next;
-    ListElement * cur2 = l2.first->next;
-
-    cut.first->next = cur;
-    cur->prev = cut.first;
 
     while(1){
         if(cur1->val < cur2->val){
-            cur->next = cur1;
-            cur1->prev = cur;
+            connect(cur, cur1);
             cur = cur1;
             if(cur == l1.last){
-                cur->next = cur2;
-                cur2->prev = cur;
+                connect(cur, cur2);
                 cut.last = l2.last;
                 return cut;
             } else
                 cur1 = cur1->next;
         } else {
-            cur->next = cur2;
-            cur2->prev = cur;
+            connect(cur, cur2);
             cur = cur2;
             if(cur == l2.last){
-                cur->next = cur1;
-                cur1->prev = cur;
+                connect(cur, cur1);
                 cut.last = l1.last;
                 return cut;
             } else
@@ -84,13 +78,22 @@ Cut merge(Cut l1, Cut l2){
 
 Cut recursion(ListElement * first, int len){
     //printf("r %i %i\n", from, to);
-    if(len > 2){
+    if(len > 3){
         int half_len = len / 2;
         Cut cut1 = recursion(first, half_len);
-        Cut cut2 = recursion(cut1.next, len - half_len);
+        Cut cut2 = recursion(cut1.next, len - half_len); // uses next
         return merge(cut1, cut2);
-    } else if(len == 1){
-        Cut cut = { first, first, first->next };
+    } else if(len > 2){
+        Cut cut = recursion(first->next, 2);
+        if(first->val < cut.first->val){
+            connect(first, cut.first);
+            cut.first = first;
+        } else if(first->val < cut.last->val){
+            connect3(cut.first, first, cut.last);
+        } else {
+            connect(cut.last, first);
+            cut.last = first;
+        }
         return cut;
     } else {
         Cut cut = { first, first->next };
@@ -107,10 +110,18 @@ Cut recursion(ListElement * first, int len){
     }
 }
 
+void merge_sort(ListElement * first, int len){
+    Cut cut = recursion(first, len);
+    head = cut.first;
+    tail = cut.last;
+    head->prev = NULL;
+    tail->next = NULL;
+}
+
 int main(){
 
     print();
-    recursion(head, list_len);
+    merge_sort(head, list_len);
     print();
 
     return 0;
